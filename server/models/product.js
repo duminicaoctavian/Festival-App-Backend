74c1964fc70d69a6b327e54b5c02c59b341bb3bb
Duminica Octavian <duminicaoctavian@Duminicas-MacBook-Pro.local>
2018-02-24 21:38:41 +0200

    added category requirement to product model

--- server/models/product.js
+++ server/models/product.js
@@ -13,6 +13,10 @@ const productSchema = mongoose.Schema({
     productImage: {
       type: String,
       required: true
+    },
+    category: {
+      type: String,
+      required: true
     }
 });
 
